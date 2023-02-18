9c324b231031bbbc2b2ecd1ecf0e0091545ee163
mzffrtp <mzffr.tpgl@gmail.com>
2023-02-18 18:39:56 +0100

    categories actions page created

--- src/pages/CategoryActions.js
+++ src/pages/CategoryActions.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import Header from "../components/header";
+import "../assets/styles/categoryactions.css"
 
 const CategoryActions = () => {
+
+const [categories, setCategories] = useState();
+const [showErrorModal, setShowErrorModal] = useState(false)
+useEffect(()=>{
+    axios
+    .get("http://localhost:3004/categories")
+    .then((res)=>{
+        setCategories(res.data)
+    })
+    .catch((err)=>{
+        setShowErrorModal(true)
+
+    })
+}, [])
+
+if (categories === null) return null;
     return (
         <div>
-            <Header whichPage={""}/>
-            category actions
+            <Header whichPage={"CategoryActions"} navigateTo= "/"/>
+            <div className="catOpCon">
+                <div className="catOpContentWrapper">
+                    {
+                        categories.length === 0 ? (
+                            <p>No categories for now!</p>
+                        ): (
+                            <>
+                            {
+                                categories.map((category)=>(
+                                    <div key={category.id}>
+                                        <p>
+                                            {category.name}
+                                        </p>
+
+                                    </div>
+                                ))
+                            }
+                            </>
+                        )
+                    }
+                    
+                </div>
+            </div>
         </div>
     )
 
